867bef94bd9ec0325ebd85d3fe9bacc2e72976d0
Rene Milk <rene.milk@uni-muenster.de>
2013-09-06 15:00:22 +0200

    introduce sessions

--- python2/runner/mountain.py
+++ python2/runner/mountain.py
@@ -24,3 +24,16 @@ class Mountain:
         self.tests(self.lesson)
         self.lesson.learn()
         return self.lesson
+      
+    def session(self, num):
+      sessions = { 1: ['about_asserts', 'about_strings', 'about_none', 'about_lists', 'about_list_assignments', 
+            'about_dictionaries', 'about_string_manipulation', 'about_tuples', 'about_methods'],
+        2: ['about_control_statements', 'about_true_and_false', 'about_sets', 'about_triangle_project', 
+            'about_exceptions', 'about_triangle_project2',],
+        3: ['about_iteration', 'about_comprehension', 'about_generators', 'about_lambdas', 'about_scoring_project',],
+        4: ['about_classes', 'about_new_style_classes', 'about_class_attributes', 'about_with_statements', 'about_dice_project',],
+        5: ['about_inheritance', 'about_multiple_inheritance', 'about_scope', 'about_modules', 'about_packages',],
+        #6: ['', '', '', '', '', '', '', '', '', '',], 
+      }
+      return self.walk_the_path(sessions[num])
+            
